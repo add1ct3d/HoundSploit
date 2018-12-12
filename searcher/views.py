07bfcb1782f5bb77ba344788fde07fa0f3e99d71
nicolas-carolo <nicolas.carolo@outlook.com>
2018-12-12 21:24:26 +0100

    Code refactor of views.py

--- searcher/views.py
+++ searcher/views.py
@@ -21,55 +21,40 @@ def get_results_table(request):
 
 def view_exploit_code(request, exploit_id):
     exploit = Exploit.objects.get(id=exploit_id)
-    exploit_description = exploit.description
-    exploit_file = exploit.file
-    exploit_author = exploit.author
-    exploit_date = exploit.date
-    exploit_type = exploit.vulnerability_type
-    exploit_platform = exploit.platform
-    exploit_port = exploit.port
-
     pwd = os.path.dirname(__file__)
     file_path = '/static/vulnerability/' + exploit.file
     with open(pwd + '/static/vulnerability/' + exploit.file, 'r') as f:
         content = f.readlines()
         vulnerability_code = ''.join(content)
     return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code,
-                                                'vulnerability_description': exploit_description,
-                                                'vulnerability_file': exploit_file,
-                                                'vulnerability_author': exploit_author,
-                                                'vulnerability_date': exploit_date,
-                                                'vulnerability_type': exploit_type,
-                                                'vulnerability_platform': exploit_platform,
-                                                'vulnerability_port': exploit_port,
+                                                'vulnerability_description': exploit.description,
+                                                'vulnerability_file': exploit.file,
+                                                'vulnerability_author': exploit.author,
+                                                'vulnerability_date': exploit.date,
+                                                'vulnerability_type': exploit.vulnerability_type,
+                                                'vulnerability_platform': exploit.platform,
+                                                'vulnerability_port': exploit.port,
                                                 'file_path': file_path,
-                                                'file_name': exploit_description + get_vulnerability_extension(exploit_file),
+                                                'file_name': exploit.description + get_vulnerability_extension(exploit.file),
                                                 })
 
 
 def view_shellcode_code(request, shellcode_id):
     shellcode = Shellcode.objects.get(id=shellcode_id)
-    shellcode_description = shellcode.description
-    shellcode_file = shellcode.file
-    shellcode_author = shellcode.author
-    shellcode_date = shellcode.date
-    shellcode_type = shellcode.vulnerability_type
-    shellcode_platform = shellcode.platform
     pwd = os.path.dirname(__file__)
     file_path = '/static/vulnerability/' + shellcode.file
     with open(pwd + '/static/vulnerability/' + shellcode.file, 'r') as f:
         content = f.readlines()
         vulnerability_code = ''.join(content)
-
     return render(request, 'code_viewer.html', {'vulnerability_code': vulnerability_code,
-                                                'vulnerability_description': shellcode_description,
-                                                'vulnerability_file': shellcode_file,
-                                                'vulnerability_author': shellcode_author,
-                                                'vulnerability_date': shellcode_date,
-                                                'vulnerability_type': shellcode_type,
-                                                'vulnerability_platform': shellcode_platform,
+                                                'vulnerability_description': shellcode.description,
+                                                'vulnerability_file': shellcode.file,
+                                                'vulnerability_author': shellcode.author,
+                                                'vulnerability_date': shellcode.date,
+                                                'vulnerability_type': shellcode.vulnerability_type,
+                                                'vulnerability_platform': shellcode.platform,
                                                 'file_path': file_path,
-                                                'file_name': shellcode_description + get_vulnerability_extension(shellcode_file),
+                                                'file_name': shellcode.description + get_vulnerability_extension(shellcode.file),
                                                 })
 
 
