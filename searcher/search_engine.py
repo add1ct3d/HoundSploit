7d3d8f5a48439238e22004831d6da94a24ed1008
nicolas-carolo <nicolas.carolo@outlook.com>
2018-12-15 11:03:22 +0100

    I have removed exact search

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -8,12 +8,6 @@ from pkg_resources import parse_version
 
 def search_vulnerabilities_in_db(search_text, db_table):
     words = (str(search_text).upper()).split()
-    if (words[0] == '--EXACT' and '--IN' in words) and db_table == 'searcher_exploit':
-        return search_exploits_exact(words[1:])
-
-    if (words[0] == '--EXACT' and '--IN' in words) and db_table == 'searcher_shellcode':
-        return search_shellcodes_exact(words[1:])
-
     if str(search_text).isnumeric():
         return search_vulnerabilities_numerical(search_text, db_table)
     elif str_is_num_version(str(search_text)) and str(search_text).__contains__(' ') and not str(search_text).__contains__('<'):
@@ -67,46 +61,6 @@ def search_vulnerabilities_for_author(search_text, db_table):
     else:
         queryset = Shellcode.objects.filter(query)
     return queryset
-
-
-# todo this method will be replaced with advanced searcher
-def search_exploits_exact(words):
-    accepted_fileds = ['FILE', 'DESCRIPTION', 'AUTHOR', 'TYPE', 'PLATFORM', 'PORT']
-    search_string = words[0]
-    words_index = 1
-    for word in words[1:]:
-        if word != '--IN':
-            search_string = search_string + ' ' + word
-            words_index = words_index + 1
-        else:
-            if words[words_index + 1] not in accepted_fileds:
-                words[words_index + 1] = 'DESCRIPTION'
-                search_string = 'blablabla'
-            if words[words_index + 1] == 'TYPE':
-                words[words_index + 1] = 'VULNERABILITY_TYPE'
-            if words[words_index + 1] == 'PORT' and search_string.isnumeric():
-                return Exploit.objects.raw('select * from searcher_exploit where port = ' + search_string.upper())
-
-            else:
-                return Exploit.objects.raw('select * from searcher_exploit where ' + words[words_index + 1] + ' like \'%' + search_string.upper() + '%\'')
-
-
-# todo this method will be replaced with advanced searcher
-def search_shellcodes_exact(words):
-    accepted_fileds = ['FILE', 'DESCRIPTION', 'AUTHOR', 'TYPE', 'PLATFORM']
-    search_string = words[0]
-    words_index = 1
-    for word in words[1:]:
-        if word != '--IN':
-            search_string = search_string + ' ' + word
-            words_index = words_index + 1
-        else:
-            if words[words_index + 1] not in accepted_fileds:
-                words[words_index + 1] = 'DESCRIPTION'
-                search_string = 'blablabla'
-            if words[words_index + 1] == 'TYPE':
-                words[words_index + 1] = 'VULNERABILITY_TYPE'
-            return Exploit.objects.raw('select * from searcher_shellcode where ' + words[words_index + 1] + ' like \'%' + search_string.upper() + '%\'')
 
 
 def is_valid_input(string):
