295492e72fd1c073989e75fdc4a0f79e2d1d245d
nicolas-carolo <nicolas.carolo@outlook.com>
2019-01-09 12:47:32 +0100

    Refactor of str_contains methods in search engine

--- searcher/search_engine.py
+++ searcher/search_engine.py
@@ -78,15 +78,17 @@ def str_contains_numbers(str):
 
 
 def str_is_num_version(str):
-    return bool(re.search(r'(\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)', str))
+    # return bool(re.search(r'(\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)', str))
+    return bool(re.search(r'[\d+(\.| |^$)]+', str))
 
 
 def str_contains_num_version_range(str):
-    return bool(re.search(r'(\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d) < (\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)', str))
+    # return bool(re.search(r'(\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d) < (\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)', str))
+    return bool(re.search(r'\d+([\.\d+]+)? < \d+([\.\d+]+)?', str))
 
 
 def str_contains_num_version_range_with_x(str):
-    return bool(re.search(r'(\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)(\.x)? < (\d\.\d\.\d\.\d|\d\.\d\.\d|\d\.\d|\d)(\.x)?', str))
+    return bool(re.search(r'\d+([\.\d+]+)? < \d+([\.\d+]+)?(\.x)? < \d+([\.\d+]+)? < \d+([\.\d+]+)?(\.x)?', str))
 
 
 def get_num_version(software_name, description):
